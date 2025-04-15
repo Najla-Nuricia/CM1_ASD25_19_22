74c64c8c0dac23841b52a448d42187960b7702f0
NAJLA NURICIA LAUDY <najlalaudy@gmail.com>
2025-04-15 09:14:52 +0700

    19_ClassBarang

---
+++ src/CM1922/barang1922.java
@@ -0,0 +1,17 @@
+package CM1922;
+
+public class barang1922 {
+    String kodeBarang1922;
+    String nama1922;
+    String kategori1922;
+    int stok1922;
+    int harga1922;
+
+    barang1922(String kodeBarang1922,String nama1922,String kategori1922,int stok1922,int harga1922) {
+        this.kodeBarang1922=kodeBarang1922;
+        this.kategori1922=kategori1922;
+        this.stok1922=stok1922;
+        this.harga1922=harga1922;
+    }
+    
+}
