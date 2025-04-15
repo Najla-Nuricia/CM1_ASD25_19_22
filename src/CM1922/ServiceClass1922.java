49a57be8f7b77b026dab08658fbcd16607c94740
NAJLA NURICIA LAUDY <najlalaudy@gmail.com>
2025-04-15 10:35:18 +0700

    19_searching

--- src/CM1922/ServiceClass1922.java
+++ src/CM1922/ServiceClass1922.java
@@ -1,10 +1,30 @@
 package CM1922;
+import java.util.Scanner;
 
 public class ServiceClass1922 {
-    ServiceClass1922[] Transaksipembelian;
+    Scanner sultanala = new Scanner(System.in);
+    transaksiPembelian1922[] trs;
     int index;
     
-    void tambahDataTransaksi22(){
+    void tambahDataTransaksi22(transaksiPembelian1922 trp){
+        System.out.println();
+    }
+
+    void searching(){
+        System.out.print("masukkan kode transaksi yang dicari: ");
+        String cari = sultanala.nextLine();
+        for (int i = 0; i < 5 ; i ++) {
+            if (trs[i].kodeTransaksi22.equalsIgnoreCase(cari)) {
+                System.out.println("trasaksi ditemukan");
+                trs[i].tampilDataTransaksi();
+                
+            }
+        }
         
+    }
+
+    void shorting(){ 
+
+
     }
 }
--- src/CM1922/transaksipembelian1922.java
+++ src/CM1922/transaksipembelian1922.java
@@ -1,12 +1,13 @@
 package CM1922;
-public class transaksipembelian1922 {
+public class transaksiPembelian1922 {
+    public static Object tampilDataTransaksi;
     String kodeTransaksi22;
     String namaPembeli22;
     String tanggalPembelian22;
     int qty22;
     barang1922 brg22;
 
-    transaksipembelian1922(String kodeTransaksi22, String namaPembeli22, String tanggalPembelian22, int qty22, barang1922 Barang22) {
+    transaksiPembelian1922(String kodeTransaksi22, String namaPembeli22, String tanggalPembelian22, int qty22, barang1922 Barang22) {
         this.kodeTransaksi22 = kodeTransaksi22;
         this.namaPembeli22 = namaPembeli22;
         this.tanggalPembelian22 = tanggalPembelian22;
