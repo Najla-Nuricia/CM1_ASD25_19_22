89aec30558d15e11e0768ef6b6d29ae413dac705
ToyamiNeko <sultanariva55@gmail.com>
2025-04-15 09:21:17 +0700

    22_transaksiPembelian

---
+++ src/transaksipembelian1922.java
@@ -0,0 +1,23 @@
+public class transaksipembelian1922 {
+    String kodeTransaksi;
+    String namaPembeli;
+    String tanggalPembelian;
+    int qty;
+    barang1922 Barang;
+
+    transaksipembelian1922(String kodeTransaksi, String namaPembeli, String tanggalPembelian, int qty, barang1922 Barang) {
+        this.kodeTransaksi = kodeTransaksi;
+        this.namaPembeli = namaPembeli;
+        this.tanggalPembelian = tanggalPembelian;
+        this.qty = qty;
+        this.Barang = Barang;
+    }
+
+    void tampilDataTransaksi() {
+        System.out.println("Kode Transaksi: " + kodeTransaksi);
+        System.out.println("Nama Pembeli: " + namaPembeli);
+        System.out.println("Tanggal Pembelian: " + tanggalPembelian);
+        System.out.println("Jumlah Barang: " + qty);
+        Barang.tampilBarang();
+    }
+}
